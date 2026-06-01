5c96117f0935976899ddecb331aeefe2f3214c3d
KingEli <toballz@yahoo.com>
2026-06-01 19:05:04 -0400

    Refactor build-compose.sh for improved readability and add build-compose.sh.bat for Windows compatibility

--- build-compose.sh
+++ build-compose.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+PROJECTNAME="global_apps"
 STAGE=${1:?STAGE is required}
 
 if [ "$STAGE" = "dev" ]; then
-  STAGE=$STAGE docker compose -p global_apps -f docker-compose.yml -f docker-compose-override.yml up -d --build
+  STAGE=$STAGE docker compose -p $PROJECTNAME -f docker-compose.yml -f docker-compose-override.yml up -d --build
 elif [ "$STAGE" = "prod" ]; then
-  STAGE=$STAGE docker compose -p global_apps -f docker-compose.yml up -d --build
+  VERSION=${2:?VERSION is required for prod}
+  STAGE=$STAGE VERSION=$VERSION docker compose -p $PROJECTNAME -f docker-compose.yml up -d
 else
-  echo "Usage: $0 [dev|prod]"
+  echo "Usage: $0 [dev|prod] [version]"
+  echo "  dev:  $0 dev"
+  echo "  prod: $0 prod 1.0.0"
   exit 1
 fi
